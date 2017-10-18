192434684a9baa0e0822115d4a986afaa7826c7b
Hans Rakers <h.rakers@global.leaseweb.com>
2017-10-18 14:28:48 +0200

    cleanups & add some informational startup msgs

--- main.go
+++ main.go
@@ -7,12 +7,29 @@ import (
 	"os/signal"
 
 	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/common/log"
+	"github.com/prometheus/common/version"
 )
 
 var (
 	listenAddress = flag.String("web.listen-address", ":9104", "Address to listen on for web interface and telemetry.")
 	metricPath    = flag.String("web.telemetry-path", "/metrics", "Path under which to expose metrics.")
 )
+
+// landingPage contains the HTML served at '/'.
+// TODO: Make this nicer and more informative.
+var landingPage = []byte(`<html>
+<head><title>Rsyslog exporter</title></head>
+<body>
+<h1>Rsyslog exporter</h1>
+<p><a href='` + *metricPath + `'>Metrics</a></p>
+</body>
+</html>
+`)
+
+func init() {
+	prometheus.MustRegister(version.NewCollector("rsyslog_exporter"))
+}
 
 func main() {
 	flag.Parse()
@@ -29,21 +46,15 @@ func main() {
 		exporter.run()
 	}()
 
+	log.Infoln("Starting rsyslog_exporter", version.Info())
+	log.Infoln("Build context", version.BuildContext())
+
 	prometheus.MustRegister(exporter)
 	http.Handle(*metricPath, prometheus.Handler())
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(`<html>
-<head><title>Rsyslog exporter</title></head>
-<body>
-<h1>Rsyslog exporter</h1>
-<p><a href='` + *metricPath + `'>Metrics</a></p>
-</body>
-</html>
-`))
+		w.Write(landingPage)
 	})
 
-	err := http.ListenAndServe(*listenAddress, nil)
-	if err != nil {
-		panic(err)
-	}
+	log.Infoln("Listening on", *listenAddress)
+	log.Fatal(http.ListenAndServe(*listenAddress, nil))
 }
