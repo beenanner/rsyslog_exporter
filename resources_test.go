57b18759d476027a584111063362ac16334276da
Hans Rakers <h.rakers@global.leaseweb.com>
2017-10-19 11:08:26 +0200

    add origin field to test input

--- resources_test.go
+++ resources_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 
 var (
-	resourceLog = []byte(`{"name":"resource-usage","utime":10,"stime":20,"maxrss":30,"minflt":40,"majflt":50,"inblock":60,"oublock":70,"nvcsw":80,"nivcsw":90,"openfiles":100}`)
+	resourceLog = []byte(`{"name":"resource-usage","origin":"impstats","utime":10,"stime":20,"maxrss":30,"minflt":40,"majflt":50,"inblock":60,"oublock":70,"nvcsw":80,"nivcsw":90,"openfiles":100}`)
 )
 
 func TestNewResourceFromJSON(t *testing.T) {
